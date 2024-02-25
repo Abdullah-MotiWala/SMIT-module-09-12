798690326826d78423f162b9e0577b4b3ebf669b
= <>
2024-02-25 11:38:05 +0500

    introduction to JS, chap 1,2,3,4,5

---
+++ app.js
@@ -0,0 +1,91 @@
+document.write("JavaScript Connected");
+
+// Variable Declaration
+// var 1abdc = "abc" // name cannot be start with numbers
+// var 234 = "abc" // name cannot be number
+// var @-/#$%&*+*/alpha = "abc" //name cannot have special characters
+// var my name = "abd" // name cannot have space init
+var abc = "abc";
+var abc_1 = "abc";
+
+var snake_case = "abc"; // snake case
+var camelCase = "abc"; // camel case
+var PascalCase = "abd"; // pascal case
+
+//  DataTypes Premitive Types
+var myString = "My name is KHAN"; // string
+var myNumber = 24; // number
+var myNumber2 = 25; // number
+var myBool = true; // boolean
+var myBool2 = false; // boolean
+var myUndefined = undefined; // undefined
+var myNull = null; //null
+
+// DataTypes Non-Premitive Types
+// var myObj2 = { name: "abdullah", age: 27 };
+var myObject = { name: "abc", age: 27, rollNo: "24-123", isUserPassed: false }; // object
+var myArray = ["abc", 123, true, undefined, null, myNull, myUndefined]; // array
+
+// STRINGS
+// (var keyword) (variable name) (assigning operator =) (variable value)
+var variableName = "variable value"; // double quotation
+var variableName2 = "variable value"; // single quotation
+
+var variableNonAssigned;
+variableNonAssigned = "assigned";
+
+var myNewString = "Two";
+// alert(myNewString)
+
+// alert(myNewString)
+
+var myNewString = "One";
+var myNewString2 = myNewString;
+// alert(myNewString2)
+
+// var myNumber = 25;
+
+// var myNumber2 = myNumber;
+// alert(myNumber2)
+
+// var myNumber = 24
+// alert(myNumber);
+
+// var halfAge = 11
+// var halfAge2 = 11
+// var myAge = 11 + 11
+// alert(myAge)
+
+// var myAgeString = "23";
+// var myAgeNumber = 23;
+
+// var allAge = myAgeNumber + myAgeString;
+
+// alert(allAge);
+// var greeting = "Hello I am good,How are you?......."
+// alert(greeting)
+// alert(greeting)
+// var versionsTill2015 = 6
+// var yearsAfter2015 = 9
+// var esVersions = versionsTill2015 + yearsAfter2015;
+// alert(esVersions);
+// var seperationFromPakInYears = 24;
+// var independenceOfPakInYears = 76;
+// var ageOfBangladesh = independenceOfPakInYears - seperationFromPakInYears;
+// alert(ageOfBangladesh);
+
+// var numberOfRows = 10;
+// var capacityOfRow = 3;
+// var totalStudents = numberOfRows * capacityOfRow;
+// alert(totalStudents);
+
+// var grapes = 5;
+// var numberOfPeople = 10;
+// var numberOfPieces = grapes / numberOfPeople;
+// alert(numberOfPieces);
+
+
+var grapes = 11;
+var numberOfPeople = 2;
+var remainder = grapes % numberOfPeople;
+alert(remainder);
