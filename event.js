09425b2d485024d81e75e198f5887121dfe64b2e
Abdullah <motiwalaabdullah100@gmail.com>
2024-03-31 11:50:14 +0500

    till 57

--- event.js
+++ event.js
@@ -1,19 +1,87 @@
-function validation(nameID, fNameID, cnicID) {
-  const nameInput = document.getElementById(nameID);
-  const fNameInput = document.getElementById(fNameID);
-  const cnicInput = document.getElementById(cnicID);
+const images = document.getElementsByClassName("image");
+const image = document.getElementById("image");
+const imageByTag = document.getElementsByTagName("img");
+// const imageByQS = document.querySelector(".image")
 
-  const nameValue = nameInput.value;
-  const fNameValue = fNameInput.value;
-  const cnicValue = cnicInput.value;
+console.log("images: ", images, "image: ", image, "tagName: ", imageByTag);
 
-  if (nameValue.length < 3) {
-    alert("Value is rquired");
-  }
+// const pic1 =
+//   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZQVDZ9nkZbVDLKgZ4l0Hl7tIjpAUIaxUgdx_PR3E4Fg&s";
+// const pic2 =
+//   "https://png.pngtree.com/png-clipart/20210722/ourmid/pngtree-mobility-tool-for-car-transportation-png-image_6561561.png";
 
-  console.log(nameValue);
-  alert("run");
-}
+// function changeImage() {
+//   const image = document.getElementById("image");
+//   image.src = pic2;
+// }
+
+// function returnImage() {
+//   const image = document.getElementById("image");
+//   image.src = pic1;
+// }
+
+// function showForm() {
+//   const loginForm = document.getElementById("loginFrom");
+//   loginForm.className = "show";
+//   console.log(loginForm);
+// }
+
+// function hide() {
+//   const modalElement = document.getElementById("modal");
+//   modalElement.className = "hide";
+//   console.log(modalElement.className);
+// }
+
+// function showMore() {
+//   const showMoreElement = document.getElementById("showMoreText");
+//   const showMoreButton = document.getElementById("showMoreButton");
+
+//   if (showMoreButton.innerText == "Show Less") {
+//     showMoreButton.innerText = "Show More";
+//     showMoreElement.innerText = "This is less tex....";
+//   } else {
+//     showMoreButton.innerText = "Show Less";
+//     showMoreElement.innerText = "This is more text with more data";
+//   }
+// }
+
+// function showMore() {
+//   const showMoreElement = document.getElementById("seeMore");
+//   const seeMoreButton = document.getElementById("seeMoreButton");
+//   const showMoreButtonText = seeMoreButton.innerText;
+//   if (showMoreButtonText === "See More") {
+//     showMoreElement.innerText = "def";
+//     seeMoreButton.innerText = "See Less";
+//   } else if (showMoreButtonText === "See Less") {
+//     showMoreElement.innerText = "abc";
+//     seeMoreButton.innerText = "See More";
+//   }
+// }
+// const parentHTML = document.getElementById("parent");
+// console.log(parentHTML.innerText);
+// console.log(parentHTML.innerHTML);
+// parentHTML.innerHTML = "Inner HTML";
+
+// const nameInput = document.getElementById("name-input");
+// console.log(nameInput.value);
+// nameInput.value = "DEF";
+
+// function validation(nameID, fNameID, cnicID) {
+//   const nameInput = document.getElementById(nameID);
+//   const fNameInput = document.getElementById(fNameID);
+//   const cnicInput = document.getElementById(cnicID);
+
+//   const nameValue = nameInput.value;
+//   const fNameValue = fNameInput.value;
+//   const cnicValue = cnicInput.value;
+
+//   if (nameValue.length < 3) {
+//     alert("Value is rquired");
+//   }
+
+//   console.log(nameValue);
+//   alert("run");
+// }
 
 // const nameInput = document.getElementById("name");
 // console.log(nameInput);
